843d80ffec2c28d6981a21915ff11cbc360e7e99
Shan21813031 <S.AUK@outlook.com>
2022-04-24 02:47:00 +0100

    user specified post view completed

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -7,8 +7,13 @@ use App\Models\Post;
 
 class Dashboard extends Controller
 {
-    public function show_post(){
-        $posts = Post::all();
+    public function show_post(Request $request){
+        // $posts = Post::all();
+        
+        $userid = $request->user()->id;
+        $posts = Post::where('user_id', $userid)->get();
+
+
         return view('dashboard', ['posts'=>$posts]);
     }
 }
